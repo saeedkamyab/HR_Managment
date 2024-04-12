9ea5c1752d2e098ffcba05f464246ac080fdd206
saeedkamyab <saeed.kamyab@yahoo.com>
2024-04-12 12:30:29 -0700

    I made all Repositories

---
+++ HR_Managment.Application/Contracts/Persistence/Common/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace HR_Managment.Application.Contracts.Persistence.Common
+{
+    public interface IBaseRepository<TKey, TEntity> where TEntity : class
+    {
+        Task<TEntity> Get(TKey id);
+        Task<IReadOnlyList<TEntity>> GetAll();
+        Task<TEntity> Add(TEntity entity);
+        Task Update(TEntity entity);
+        Task Delete(TKey id);
+    }
+}
---
+++ HR_Managment.Application/Contracts/Persistence/ILeaveRequestRepository.cs
@@ -0,0 +1,9 @@
+﻿using HR_Managment.Application.Contracts.Persistence.Common;
+using HR_Managment.Domain.Models;
+
+namespace HR_Managment.Application.Contracts.Persistence
+{
+    public interface ILeaveRequestRepository:IBaseRepository<int,LeaveRequest>
+    {
+    }
+}
