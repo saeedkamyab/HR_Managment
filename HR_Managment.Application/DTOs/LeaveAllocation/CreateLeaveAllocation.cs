72c6f576c6dcab02f7440ddc5a432fbe12dc29a2
saeedkamyab <saeed.kamyab@yahoo.com>
2024-06-30 09:48:08 +0330

    video 18 is finished

---
+++ HR_Managment.Application/DTOs/LeaveAllocation/CreateLeaveAllocation.cs
@@ -0,0 +1,16 @@
+﻿using HR_Managment.Application.DTOs.LeaveType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.DTOs.LeaveAllocation
+{
+    public class CreateLeaveAllocation:BaseDto<int>
+    {
+        public int DayCount { get; set; }
+        public int Period { get; set; }
+        public int LeaveTypeId { get; set; }
+    }
+}
--- HR_Managment.Application/DTOs/LeaveRequest/LeaveRequestDto.cs
+++ HR_Managment.Application/DTOs/LeaveRequest/LeaveRequestDto.cs
@@ -9,7 +9,7 @@ namespace HR_Managment.Application.DTOs.LeaveRequest
         public DateTime EndDate { get; set; }
         public DateTime DateRequested { get; set; }
         public string RequestComments { get; set; }
-        public DateTime DateActioned { get; set; }
+        public DateTime? DateActioned { get; set; }
         public bool? Approved { get; set; }
         public bool Cancelled { get; set; }
 
