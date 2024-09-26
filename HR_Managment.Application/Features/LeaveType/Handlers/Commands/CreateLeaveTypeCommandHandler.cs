a55d942838db36b79a34680b458d72b15ddb6031
saeedkamyab <saeed.kamyab@yahoo.com>
2024-09-26 17:42:28 +0330

    first validator has been used

--- HR_Managment.Application/Features/LeaveType/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
+++ HR_Managment.Application/Features/LeaveType/Handlers/Commands/CreateLeaveTypeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR_Managment.Application.Contracts.Persistence;
+using HR_Managment.Application.DTOs.LeaveType.Validator;
 using HR_Managment.Application.Features.LeaveType.Requests.Commands;
 using MediatR;
 
@@ -18,6 +19,11 @@ namespace HR_Managment.Application.Features.LeaveType.Handlers.Commands
 
         public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
         {
+            var validator=new CreateLeaveTypeDtoValidator();
+            var validationResult=await validator.ValidateAsync(request.LeaveTypeDto, cancellationToken);
+            if (!validationResult.IsValid) {
+                throw new Exception();
+            }
             var leaveType = _mapper.Map<Domain.Models.LeaveType>(request.LeaveTypeDto);
             leaveType=await _leaveTypeRepository.Add(leaveType);
             return leaveType.Id;
