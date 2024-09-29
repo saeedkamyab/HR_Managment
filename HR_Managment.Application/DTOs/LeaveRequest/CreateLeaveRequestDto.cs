04d8b3f79859f891619b402a1407f39cd1880f79
saeedkamyab <saeed.kamyab@yahoo.com>
2024-09-29 21:02:33 +0330

    new validator has been added

--- HR_Managment.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
+++ HR_Managment.Application/DTOs/LeaveRequest/CreateLeaveRequestDto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HR_Managment.Application.DTOs.LeaveType;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,16 @@ namespace HR_Managment.Application.DTOs.LeaveRequest
 {
     public class CreateLeaveRequestDto:BaseDto<int>
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime DateRequested { get; set; }
+        public string? RequestComments { get; set; }
+        public DateTime? DateActioned { get; set; }
+        public bool? Approved { get; set; }
+        public bool Cancelled { get; set; }
 
+        [Required]
+        public required CreateLeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
     }
 }
---
+++ HR_Managment.Application/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using HR_Managment.Application.Contracts.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Application.DTOs.LeaveRequest.Validators
+{
+    public class CreateLeaveRequestDtoValidator:AbstractValidator<CreateLeaveRequestDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            RuleFor(p => p.LeaveTypeId).GreaterThan(0).MustAsync(async (id, token) =>
+            {
+                var existingLeaveType = await _leaveTypeRepository.Get(id);
+                if (existingLeaveType != null) return false; else return true;
+            }).WithMessage("{PropertyName} does not exist.");
+        }
+
+    }
+}
