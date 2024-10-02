74d2137f0f2440ce3136f698f051375df7794068
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-02 12:29:48 +0330

    Infrastructure and Persistance have been initilized

---
+++ HR_Managment.Persistance/LeaveManagmentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HR_Managment.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Managment.Persistance
+{
+    public class LeaveManagmentDbContext:DbContext
+    {
+        public LeaveManagmentDbContext(DbContextOptions<LeaveManagmentDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
