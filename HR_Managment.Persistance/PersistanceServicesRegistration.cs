b6c9f4503ac4aef25a79a63603767d28115d52f2
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-04 18:45:18 +0330

    re

---
+++ HR_Managment.Persistance/PersistanceServicesRegistration.cs
@@ -0,0 +1,36 @@
+﻿using HR_Managment.Application.Contracts.Persistence;
+using HR_Managment.Application.Contracts.Persistence.Common;
+using HR_Managment.Domain.Models;
+using HR_Managment.Persistance.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Managment.Persistance
+{
+    public class PersistanceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistanceServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagmentDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("connectionString"));
+            });
+            
+            services.AddScoped(typeof(IBaseRepository<>), typeof(GenericRepository<>));
+           
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            
+            return services;
+        }
+
+    }
+}
