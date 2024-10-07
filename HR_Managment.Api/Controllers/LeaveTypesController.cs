7e948df686b0f1e097f21595d2daf3289c62d1a5
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-07 11:48:57 +0330

    52 finished

--- HR_Managment.Api/Controllers/LeaveTypesController.cs
+++ HR_Managment.Api/Controllers/LeaveTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using HR_Managment.Application.DTOs.LeaveType;
+using HR_Managment.Application.Features.LeaveType.Requests.Commands;
 using HR_Managment.Application.Features.LeaveType.Requests.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -27,27 +28,40 @@ namespace HR_Managment.Api.Controllers
 
         // GET api/<LeaveTypesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<LeaveTypeDto>>Get(int id)
         {
-            return "value";
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailsRequest
+            {
+                Id = id
+            });
+            return Ok(leaveType);
         }
 
         // POST api/<LeaveTypesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult>Post([FromBody] CreateLeaveTypeDto createLeaveType)
         {
+            var command = new CreateLeaveTypeCommand { LeaveTypeDto = createLeaveType };
+            var response= await _mediator.Send(command);
+            return Ok(response); 
         }
 
         // PUT api/<LeaveTypesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult> Put(int id, [FromBody] CreateLeaveTypeDto leaveType)
         {
+            var command = new UpdateLeaveTypeCommand { LeaveTypeDto = leaveType };
+            await _mediator.Send(command);
+            return NoContent();
         }
 
         // DELETE api/<LeaveTypesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            var command = new DeleteLeaveTypeCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
         }
     }
 }
